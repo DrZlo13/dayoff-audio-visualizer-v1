1bab1b1bf6ef15e0dbe9cf89a2b216d7a5968b90
SG <who.just.the.doctor@gmail.com>
2023-03-05 02:40:06 +0300

    README

--- src/App.svelte
+++ src/App.svelte
@@ -187,9 +187,52 @@
       _p5.endShape();
     };
 
+    _p5.windowResized = () => {
+      _p5.resizeCanvas(_p5.windowWidth, _p5.windowHeight);
+      width = innerWidth;
+      height = innerHeight;
+      max_width = Math.min(width, height);
+    };
+
+    let fullscreen = false;
+    function openFullscreen(elem) {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) {
+        /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) {
+        57;
+        /* IE11 */
+        elem.msRequestFullscreen();
+      }
+      fullscreen = true;
+    }
+
+    function closeFullscreen() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        /* Safari */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        /* IE11 */
+        document.msExitFullscreen();
+      }
+      fullscreen = false;
+    }
+
     _p5.mouseClicked = () => {
       if (audio_ctx.state !== "running") {
         audio_ctx.resume();
+      }
+
+      var elem = document.documentElement;
+
+      if (fullscreen) {
+        closeFullscreen();
+      } else {
+        openFullscreen(elem);
       }
     };
   };
