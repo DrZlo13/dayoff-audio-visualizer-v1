98f47bf7e133b2ac6058d6f2ee10d555d5f05dc3
DrunkBatya <drunkbatya.js@gmail.com>
2023-03-07 17:34:05 +0300

    Fix assets path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/dayoff-audio-visualizer-v1/',
   // build: {
   //   rollupOptions: {
   //     external: ['p5'],
